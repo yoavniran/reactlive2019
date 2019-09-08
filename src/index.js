a776cb2c9cd6355a7b67ac4e3d66e38be1b3b74a
yoavniran <yoavniran@gmail.com>
2019-09-08 15:29:32 +0300

    react spinners

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-// import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 import {Provider} from "react-redux";
 import getStore from "./store/store";
 
@@ -12,10 +11,4 @@ window.__store = store;
 
 ReactDOM.render(<Provider store={store}>
 	<App />
-</Provider>, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
-//
+</Provider>, document.getElementById("root"));
