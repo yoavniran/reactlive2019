b28ac3e5d9b7835efe589e3acb06e6cf1c0881b9
yoavniran <yoavniran@gmail.com>
2019-09-10 17:00:56 +0300

    cytest

--- cypress/integration/perf_spec.js
+++ cypress/integration/perf_spec.js
@@ -1,1 +1,30 @@
 //cytest
+import { TYPES } from "../../src/consts";
+
+const INTERACTION_MAX = 16;
+
+describe("reactlive2019 - perf test", () => {
+	it("test select perf", () => {
+
+		cy.visit("http://localhost:3000/?profile");
+
+		cy.window()
+			.then((win) => {
+				cy.wait(2000);
+
+				const firstGridPhoto =
+					cy.get("#root .grid-photo").first();
+
+				firstGridPhoto.find("img")
+					.click()
+					.should(() => {
+
+						const events = win._cyProfile[TYPES.SET_SELECTED_PHOTO];
+
+						expect(events[0].actualDuration)
+							.to.be.at.most(INTERACTION_MAX);
+					});
+			});
+
+	});
+});
